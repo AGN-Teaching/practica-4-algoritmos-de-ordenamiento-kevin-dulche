1b50e09cdb12ef5846a1be641c13dc66718fe8ee
Kevin-Dulche <kevdulche@gmail.com>
2024-01-30 19:54:39 -0600

    Agrego csv a markdown

---
+++ analisis/csv_a_markdown.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Cargar el archivo CSV
+df = pd.read_csv('./desviacion.csv')
+
+# Convertir el DataFrame a formato Markdown
+tabla_markdown = df.to_markdown(index=False)
+
+# Imprimir la tabla Markdown
+print(tabla_markdown)
